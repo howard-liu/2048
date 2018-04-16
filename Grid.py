24b66c6f4a19d631c108bfffc29d7ee49102be7f
howa9999 <hwd.liu@gmail.com>
2018-04-16 14:15:08 +1000

    2048 Complete - like actual game

--- Grid.py
+++ Grid.py
@@ -3,19 +3,18 @@ import random
 
 class Grid(object):
 
-    ROWS = 4
-    COLUMNS = 4
-    MAX_LENGTH = 5
+    SIZE = 4
+    MAX_LENGTH = 7
 
     def __init__(self):
-        self.grid = [[0 for x in range(self.COLUMNS)] for y in range(self.ROWS)]
+        self.grid = [[0 for x in range(self.SIZE)] for y in range(self.SIZE)]
 
     def __str__(self):
         out = ''
-        for y in range(self.ROWS):
-            for x in range(self.COLUMNS):
+        for y in range(self.SIZE):
+            for x in range(self.SIZE):
                 out = out + str(self.grid[y][x]).ljust(self.MAX_LENGTH)
-            if y != self.ROWS - 1:
+            if y != self.SIZE - 1:
                 out = out + '\n'
         return out
 
@@ -28,14 +27,14 @@ class Grid(object):
             return 2
 
     def start_game(self):
-        rand_row1 = random.randint(0, self.ROWS - 1)
-        rand_row2 = random.randint(0, self.ROWS - 1)
-        rand_column1 = random.randint(0, self.COLUMNS - 1)
-        rand_column2 = random.randint(0, self.COLUMNS - 1)
+        rand_row1 = random.randint(0, self.SIZE - 1)
+        rand_row2 = random.randint(0, self.SIZE - 1)
+        rand_column1 = random.randint(0, self.SIZE - 1)
+        rand_column2 = random.randint(0, self.SIZE - 1)
         # Make sure that coords are not the same
         while rand_row1 == rand_row2 and rand_column1 == rand_column2:
-            rand_row2 = random.randint(0, self.ROWS - 1)
-            rand_column2 = random.randint(0, self.COLUMNS - 1)
+            rand_row2 = random.randint(0, self.SIZE - 1)
+            rand_column2 = random.randint(0, self.SIZE - 1)
         self.grid[rand_row1][rand_column1] = self.random_number()
         self.grid[rand_row2][rand_column2] = self.random_number()
 
@@ -62,138 +61,231 @@ class Grid(object):
             else:
                 print('Please input w,a,s, or d')
 
-    def merge(self, row_or_col, type_number):
-        a = 0
-        b = 0
-        if row_or_col == 'row':
-            a = 1
-            total = self.ROWS
-        else:
-            b = 1
-            total = self.COLUMNS
+    # def merge_all(self, row_or_col):
+    #     for x in range(self.SIZE):
+    #         self.merge(row_or_col, x)
+    #     return
 
-        for x in range(total - 1):
-            if self.grid[x * b + type_number * a][x * a + type_number * b] == \
-                    self.grid[(x + 1) * b + type_number * a][(x + 1) * a + type_number * b]:
+    # def merge(self, row_or_col, type_number):
+    #     a = 0
+    #     b = 0
+    #     if row_or_col == 'row':
+    #         a = 1
+    #     else:
+    #         b = 1
+    #     # Check for 0s in between what we want
+    #     for x in range(self.SIZE - 1):
+    #         for y in range(self.SIZE - x):
+    #             if self.grid[x * b + type_number * a][x * a + type_number * b] == \
+    #                     self.grid[(x + y) * b + type_number * a][(x + y) * a + type_number * b]:
+    #                 self.grid[x * b + type_number * a][x * a + type_number * b] = \
+    #                     self.grid[x * b + type_number * a][x * a + type_number * b] * 2
+    #                 self.grid[(x + y) * b + type_number * a][(x + y) * a + type_number * b] = 0
+    #                 break
+    #             elif self.grid[x * b + type_number * a][x * a + type_number * b] != \
+    #                     self.grid[(x + y) * b + type_number * a][(x + y) * a + type_number * b]:
+    #                 break
+    #     return
 
+    def merge_all_rows_left(self):
+        count = 0
+        for x in range(self.SIZE):
+            count += self.merge_row_left(x)
+        return count
 
-                self.grid[x * b + type_number * a][x * a + type_number * b] = \
-                    self.grid[x * b + type_number * a][x * a + type_number * b] * 2
-                self.grid[(x + 1) * b + type_number * a][(x + 1) * a + type_number * b] = 0
-        return
+    def merge_all_rows_right(self):
+        count = 0
+        for x in range(self.SIZE):
+            count += self.merge_row_right(x)
+        return count
 
-    def move_direction(self, direction):
-        if direction == 'up' or direction == 'down':
-            first = self.COLUMNS
-            second = self.ROWS
-            row_col = 'column'
-        else:
-            first = self.ROWS
-            second = self.COLUMNS
-            row_col = 'row'
+    def merge_all_columns_up(self):
+        count = 0
+        for x in range(self.SIZE):
+            count += self.merge_column_up(x)
+        return count
 
-        for y in range(first):
-            # self.merge(row_col, y)
-            for x in range(1, second):
+    def merge_all_columns_down(self):
+        count = 0
+        for x in range(self.SIZE):
+            count += self.merge_column_down(x)
+        return count
+
+    def merge_row_left(self, row_number):
+        count = 0
+        for x in range(self.SIZE - 1):  # 0, 1, 2
+            if self.grid[row_number][x] != 0:
+                for y in range(1, self.SIZE - x):  # if x0: 1-3, if x2: 1
+                    if self.grid[row_number][x] == self.grid[row_number][x + y]:
+                        self.grid[row_number][x] = self.grid[row_number][x] * 2
+                        self.grid[row_number][x + y] = 0
+                        count = 1
+                        break
+                    elif self.grid[row_number][x + y] != 0 and \
+                            self.grid[row_number][x] != self.grid[row_number][x + y]:
+                        break
+        return count
+
+    def merge_column_up(self, column_number):
+        count = 0
+        for x in range(self.SIZE - 1):
+            if self.grid[x][column_number] != 0:
+                for y in range(1, self.SIZE - x):
+                    if self.grid[x][column_number] == self.grid[x + y][column_number]:
+                        self.grid[x][column_number] = self.grid[x][column_number] * 2
+                        self.grid[x + y][column_number] = 0
+                        count = 1
+                        break
+                    elif self.grid[x + y][column_number] != 0 and \
+                            self.grid[x][column_number] != self.grid[x + y][column_number]:
+                        break
+        return count
+
+    def merge_column_down(self, column_number):
+        count = 0
+        for x in reversed(range(1, self.SIZE)):  # 3, 2, 1
+            if self.grid[x][column_number] != 0:
+                for y in range(1, x + 1):  # if x3: 1-3; if x1: 1
+                    if self.grid[x][column_number] == self.grid[x - y][column_number]:
+                        self.grid[x][column_number] = self.grid[x][column_number] * 2
+                        self.grid[x - y][column_number] = 0
+                        count = 1
+                        break
+                    elif self.grid[x - y][column_number] != 0 and \
+                            self.grid[x][column_number] != self.grid[x - y][column_number]:
+                        break
+        return count
+
+    def merge_row_right(self, row_number):
+        count = 0
+        for x in reversed(range(1, self.SIZE)):
+            if self.grid[row_number][x] != 0:
+                for y in range(1, x + 1):
+                    if self.grid[row_number][x] == self.grid[row_number][x - y]:
+                        self.grid[row_number][x] = self.grid[row_number][x] * 2
+                        self.grid[row_number][x - y] = 0
+                        count = 1
+                        break
+                    elif self.grid[row_number][x - y] != 0 and \
+                            self.grid[row_number][x] != self.grid[row_number][x - y]:
+                        break
+        return count
+
+    def move_up(self):
+        count = 0
+        # For each column: up
+        # Starting from 2nd most 'up' part (smallest)
+        for y in range(self.SIZE):
+            for x in range(1, self.SIZE):
                 if self.grid[x][y] != 0:
-                    # Look up one step at a time, check for collisions
+                    # Keep checking upwards until does not exist or hits a number
                     for z in range(1, x + 1):
-                        if self.grid[x - z][y] != 0:
-                            self.grid[x - z + 1][y] = self.grid[x][y]
-                            if z - 1 != 0:
-                                self.grid[y][x] = 0
-                            break
-                        elif x - z == 0:
-                            self.grid[x - z][y] = self.grid[x][y]
+                        if x - z == 0 and self.grid[x - z][y] == 0:
+                            self.grid[0][y] = self.grid[x][y]
                             self.grid[x][y] = 0
+                            count = 1
                             break
+                        elif self.grid[x - z][y] != 0 and z != 1:
+                            self.grid[x - (z - 1)][y] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x - z][y] != 0:
+                            break
+        return count
+
+    def move_down(self):
+        count = 0
+        # For each column: down
+        # Starting from 2nd most 'down' part (down)
+        for y in range(self.SIZE):
+            for x in reversed(range(self.SIZE - 1)):
+                if self.grid[x][y] != 0:
+                    # Keep checking downwards until does not exist or hits a number
+                    for z in range(1, self.SIZE - x):
+                        if x + z == self.SIZE - 1 and self.grid[x + z][y] == 0:
+                            self.grid[self.SIZE - 1][y] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x + z][y] != 0 and z != 1:
+                            self.grid[x + (z - 1)][y] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x + z][y] != 0:
+                            break
+        return count
+
+    def move_left(self):
+        count = 0
+        # For each row (x)
+        # Starting from 2nd most 'left' part (smallest) (column) (y)
+        for x in range(self.SIZE):
+            for y in range(1, self.SIZE):
+                if self.grid[x][y] != 0:
+                    # Keep checking left until does not exist or hits a number
+                    for z in range(1, y + 1):
+                        if y - z == 0 and self.grid[x][y - z] == 0:
+                            self.grid[x][0] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x][y - z] != 0 and z != 1:
+                            self.grid[x][y - (z - 1)] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x][y - z] != 0:
+                            break
+        return count
+
+    def move_right(self):
+        count = 0
+        # For each row (x)
+        # Starting from 2nd most 'right' part (largest) (column) (y)
+        for x in range(self.SIZE):
+            for y in reversed(range(self.SIZE - 1)):
+                if self.grid[x][y] != 0:
+                    # Keep checking right until does not exist or hits a number
+                    for z in range(1, self.SIZE - y):
+                        if y + z == self.SIZE - 1 and self.grid[x][y + z] == 0:
+                            self.grid[x][self.SIZE - 1] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x][y + z] != 0 and z != 1:
+                            self.grid[x][y + (z - 1)] = self.grid[x][y]
+                            self.grid[x][y] = 0
+                            count = 1
+                            break
+                        elif self.grid[x][y + z] != 0:
+                            break
+        return count
 
     def move(self):
         direction = self.ask_for_direction()
-        # If up: move all pieces to minimum rows
+        count = 0
         if direction == 'up':
-            # Start from top row
-            for y in range(self.COLUMNS):
-                # self.merge('column', y)
-                for x in range(1, self.ROWS):
-                    if self.grid[x][y] != 0:
-                        # Look up one step at a time, check for collisions
-                        for z in range(1, x + 1):
-                            if self.grid[x - z][y] != 0:
-                                self.grid[x - z + 1][y] = self.grid[x][y]
-                                if z - 1 != 0:
-                                    self.grid[y][x] = 0
-                                break
-                            elif x - z == 0:
-                                self.grid[x - z][y] = self.grid[x][y]
-                                self.grid[x][y] = 0
-                                break
-
-        if direction == 'down':
-            # Start from bottom row (largest)
-            for y in range(self.COLUMNS):
-                # self.merge('column', y)
-                for x in range(1, self.ROWS):
-                    # Added swap direction
-                    x = self.ROWS - 1 - x
-                    print(x)
-                    if self.grid[x][y] != 0:
-                        # Look up one step at a time, check for collisions
-                        for z in range(1, self.ROWS - x):
-                            if self.grid[x + z][y] != 0:
-                                self.grid[x + z - 1][y] = self.grid[x][y]
-                                if z - 1 != 0:
-                                    self.grid[y][x] = 0
-                                break
-                            elif x + z == self.ROWS - 1:
-                                self.grid[x + z][y] = self.grid[x][y]
-                                self.grid[x][y] = 0
-                                break
-
-        if direction == 'left':
-            # Start from top row
-            for y in range(self.ROWS):
-                # self.merge('row', y)
-                for x in range(1, self.COLUMNS):
-                    if self.grid[y][x] != 0:
-                        # Look up one step at a time, check for collisions
-                        for z in range(1, x + 1):
-                            if self.grid[y][x - z] != 0:
-                                self.grid[y][x - z + 1] = self.grid[y][x]
-                                if z - 1 != 0:
-                                    self.grid[y][x] = 0
-                                break
-                            elif x - z == 0:
-                                self.grid[y][x - z] = self.grid[y][x]
-                                self.grid[y][x] = 0
-                                break
-
-        if direction == 'right':
-            # Start from bottom row (largest)
-            for y in range(self.ROWS):
-                # self.merge('row', y)
-                for x in range(1, self.COLUMNS):
-                    # Added swap direction
-                    x = self.COLUMNS - 1 - x
-                    if self.grid[y][x] != 0:
-                        # Look up one step at a time, check for collisions
-                        for z in range(1, self.COLUMNS - x):
-                            if self.grid[y][x + z] != 0:
-                                self.grid[y][x + z - 1] = self.grid[y][x]
-                                if z - 1 != 0:
-                                    self.grid[y][x] = 0
-                                break
-                            elif x + z == self.COLUMNS - 1:
-                                self.grid[y][x + z] = self.grid[y][x]
-                                self.grid[y][x] = 0
-                                break
-
-        self.random_number_at_a_space()
+            count += self.merge_all_columns_up()
+            count += self.move_up()
+        elif direction == 'down':
+            count += self.merge_all_columns_down()
+            count += self.move_down()
+        elif direction == 'left':
+            count += self.merge_all_rows_left()
+            count += self.move_left()
+        elif direction == 'right':
+            count += self.merge_all_rows_right()
+            count += self.move_right()
+        # Invalid move if count == 0
+        if count != 0:
+            self.random_number_at_a_space()
 
     def random_number_at_a_space(self):
         storage = []
-        for x in range(self.ROWS):
-            for y in range(self.COLUMNS):
+        for x in range(self.SIZE):
+            for y in range(self.SIZE):
                 if self.grid[x][y] == 0:
                     storage.append((x, y))
         coords = random.choice(storage)
@@ -201,16 +293,16 @@ class Grid(object):
         return
 
     def check_loss(self):
-        for x in range(self.ROWS):
-            for y in range(self.COLUMNS):
+        for x in range(self.SIZE):
+            for y in range(self.SIZE):
                 if self.grid[x][y] == 0:
                     return False
         return True
 
     def find_score(self):
         score = 0
-        for x in range(self.ROWS):
-            for y in range(self.COLUMNS):
+        for x in range(self.SIZE):
+            for y in range(self.SIZE):
                 if self.grid[x][y] > score:
                     score = self.grid[x][y]
         return score
